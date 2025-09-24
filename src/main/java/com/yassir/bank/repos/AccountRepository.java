a5e0a877a5f6ff28b5fdc1d14b9945a61b982e2b
dbu <dbu@contraste.com>
2025-09-24 18:11:49 +0100

    find acc by user

--- src/main/java/com/yassir/bank/repos/AccountRepository.java
+++ src/main/java/com/yassir/bank/repos/AccountRepository.java
@@ -1,9 +1,14 @@
 package com.yassir.bank.repos;
 
 import com.yassir.bank.model.Account;
+import com.yassir.bank.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface AccountRepository extends JpaRepository<Account,Long> {
+
+    List<Account> findAccountsByUser(User user);
 }
