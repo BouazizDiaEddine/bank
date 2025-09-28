257a509517fe7f64f061914d9c5a938a97afa2ea
dbu <dbu@contraste.com>
2025-09-28 14:00:59 +0100

    fix exchange

--- src/main/java/com/yassir/bank/exchange/ExchangeService.java
+++ src/main/java/com/yassir/bank/exchange/ExchangeService.java
@@ -2,6 +2,7 @@ package com.yassir.bank.exchange;
 
 import com.yassir.bank.currency.Currency;
 import com.yassir.bank.currency.CurrencyRepository;
+import com.yassir.bank.exception.InvalidInputException;
 import com.yassir.bank.exception.ResourceNotFoundException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -38,21 +39,29 @@ public class ExchangeService {
         reverseExchange.setToCurrency(currencyFrom);
         reverseExchange.setExchangeRate(new BigDecimal(1).divide(exchange.getExchangeRate()));
 
+        exchange.setExchangeId(null);
+        reverseExchange.setExchangeId(null);
+
         return exchangeRepository.saveAll(List.of(exchange,reverseExchange));
     }
 
     //update updated by currencyFrom to currencyTo (this updates both)
     public List<Exchange> updateExchange(Long id,Exchange updated) {
-        log.info("checking the existence if the currencies of "+updated.toString());
-        Currency currencyFrom = currencyRepository.findById(updated.getFromCurrency().getCurrencyId()).orElseThrow(() -> new ResourceNotFoundException("Currency from not found " + updated.getFromCurrency().getCurrencyId()));
-        Currency currencyTo = currencyRepository.findById(updated.getToCurrency().getCurrencyId()).orElseThrow(() -> new ResourceNotFoundException("Currency not found " + updated.getToCurrency().getCurrencyId()));
+        log.info("checking the existence if the currencies of and the exchange "+updated.toString());
 
         Exchange exists = exchangeRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("exchange for the given currencies not found" + updated.getFromCurrency().getCurrencyId()+" "+updated.getToCurrency().getCurrencyId())) ;
+        Currency currencyFrom = currencyRepository.findById(exists.getFromCurrency().getCurrencyId()).orElseThrow(() -> new ResourceNotFoundException("Currency from not found " + updated.getFromCurrency().getCurrencyId()));
+        Currency currencyTo = currencyRepository.findById(exists.getToCurrency().getCurrencyId()).orElseThrow(() -> new ResourceNotFoundException("Currency not found " + updated.getToCurrency().getCurrencyId()));
+
+        if(!exists.getToCurrency().equals(updated.getToCurrency()) ||
+                !exists.getFromCurrency().equals(updated.getFromCurrency()))
+            throw new InvalidInputException("Currencies of the body "+updated+"dont match the ones with the id :" +id);
+
         exists.setExchangeRate(updated.getExchangeRate());
 
         //update reverse
         Exchange existsReverse = exchangeRepository.findByFromCurrencyAndToCurrency(currencyTo,currencyFrom);
-        System.out.println(updated.getExchangeRate());
+
         existsReverse.setExchangeRate(new BigDecimal(1).divide(updated.getExchangeRate(),10, RoundingMode.HALF_UP));
 
         return exchangeRepository.saveAll(List.of(exists,existsReverse));
